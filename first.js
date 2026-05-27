5acd80103a7ee36bf79321bd8fee39aa76963f02
tiwarianish1830 <tiwarianish1830@gmail.com>
2026-05-27 10:49:21 +0530

    publish code on repo

---
+++ first.js
@@ -0,0 +1,15 @@
+//  console.log("anish tiwari");
+//  console.log("how are you"); 
+//  console("i love programing");
+ fullname = "anish tiwari";
+ console.log(fullname);
+ age = 25;
+ price = 99.99;
+ console.log(age);
+ console.log(price);
+ x = null;
+ y = undefined ;
+ console.log(x);
+ console.log(y);
+ isfollow = true ;   // it is a boolean value
+ console.log(isfollow);
